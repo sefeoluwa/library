2b8a20f38facf610d9733f146ca93da2f11af3d5
sefeoluwa <sefeoluwa01@gmail.com>
2023-04-13 19:15:39 +0100

    library output displayed in display container

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ const form = document.getElementById('book-form');
 const bookDisplay = document.querySelector('.book-display');
 const bookDisplayContainer = document.querySelector('.display-container')
 
-//array to store all book elements
-let myLibrary = [];
-
+const myLibrary = [];
 // constructor function to get book info
 class Book {
     constructor(title, author, pages, read) {
@@ -16,7 +14,12 @@ class Book {
     }
 };
 
-function updateLibrary(){
+Book.prototype.info = function() {
+    const readStatus = this.read ? "read" : "Not read yet";
+    return `<p class="result-title">Title: "${this.title}"</p><p class="result-author">Author: ${this.author}</p><p class="result-pages">Pages: ${this.pages}</p><p class="result-read-status"> ${readStatus}</p>`;
+  };
+
+/*function updateLibrary(){
     form.addEventListener("submit", (event) => {
         event.preventDefault(); 
     
@@ -34,23 +37,97 @@ function updateLibrary(){
         const readStatus = this.read ? "read" : "Not read yet";
         return `<p class="result-title">Title: "${this.title}"</p><p class="result-author">Author: ${this.author}</p class="result-pages"><p>Pages: ${this.pages}</p><p class="result-read-status"> ${readStatus}</p>`;
       };
+
+      myLibrary.push(book);
       
       //append the form results to the display area
       bookDisplay.innerHTML = book.info();
       
+      // reset the form
+    form.reset();
+
       //remove display of the form and display the book log
       form.style.display = 'none';
       bookDisplayContainer.style.display = 'block';
         bookDisplay.style.display = 'flex';
-     
+        
     })
     
       
       //show form element on click of the add book button
     
-}
+} */
 
+function addBookToLibrary() {
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const read = document.querySelector('#read').checked;
+  
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+  }
+  
+
+
+function updateLibrary() {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+  
+      // Get the values of the input fields
+      const title = document.querySelector("#title").value;
+      const author = document.querySelector("#author").value;
+      const pages = document.querySelector("#pages").value;
+      const read = document.querySelector("#read").checked;
+  
+      // Create a new Book object with the input values
+      const book = new Book(title, author, pages, read);
+  
+      // Add the new book object to the myLibrary array
+      myLibrary.push(book);
+  
+      // Create a new div element for the display-container
+      const displayContainer = document.createElement("div");
+      displayContainer.classList.add("display-container");
+
+  
+      // Create a new div element for each book in the myLibrary array
+      bookDisplayContainer.innerHTML = "";
+      for (let i = 0; i < myLibrary.length; i++) {
+        const bookInfo = myLibrary[i].info();
+        const bookElement = document.createElement("div");
+        
+        bookElement.innerHTML = bookInfo;
+        displayContainer.appendChild(bookElement);
+      }
+  
+      // Replace the existing display-container with the new one
+      const existingDisplayContainer = document.querySelector(".display-container");
+      if (existingDisplayContainer) {
+        existingDisplayContainer.replaceWith(displayContainer);
+      } else {
+        bookDisplay.appendChild(displayContainer);
+      }
+
+      if (myLibrary.length >1){
+        displayContainer.style.display = 'none'
+      }
+
+      
+  
+      // Clear the form input fields
+      form.reset();
+      form.style.display = 'none';
+    });
+  }
+  
+
+
+//adding a function to load the operational function for the library function when the add button is clicked
 addBtn.addEventListener('click', () => {    
     form.style.display = 'block';
    updateLibrary();
   });
+
+  //log the update library function so the user can add other books
+  
