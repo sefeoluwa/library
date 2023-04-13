afcc4c61f70b0cbf54d91efa4beb28dcd1179a97
sefeoluwa <sefeoluwa01@gmail.com>
2023-04-13 14:30:50 +0100

    add book button now displays form

--- script.js
+++ script.js
@@ -1,7 +1,54 @@
+const addBtn = document.getElementById('add-book');
+const form = document.getElementById('book-form');
 const bookDisplay = document.querySelector('.book-display');
-const btn = documenent.querySelector('add-book');
+const bookDisplayContainer = document.querySelector('.display-container')
 
-//add event listener to load a prompt when the button is clicked
-btn.addEventListener('click', function(){
-    
+//array to store all book elements
+let myLibrary = [];
+
+// constructor function to get book info
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;        
+    }
+};
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+  
+    //select the values returned from the input elements
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const read = document.querySelector('#read').checked;
+
+
+  // create a new Book object with the input values
+  const book = new Book(title, author, pages, read);
+  Book.prototype.info = function() {
+    const readStatus = this.read ? "read" : "Not read yet";
+    return `<p class="result-title">Title: "${this.title}"</p><p class="result-author">Author: ${this.author}</p class="result-pages"><p>Pages: ${this.pages}</p><p class="result-read-status"> ${readStatus}</p>`;
+  };
+  
+  //append the form results to the display area
+  bookDisplay.innerHTML = book.info();
+  
+  //remove display of the form and display the book log
+  form.style.display = 'none';
+  bookDisplayContainer.style.display = 'block';
+    bookDisplay.style.display = 'block';
+ 
 })
+
+  
+  //show form element on click of the add book button
+addBtn.addEventListener('click', () => {
+    form.reset();
+    
+    form.style.display = 'block';
+   
+  });
