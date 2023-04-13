78393cac528e71b52667fc02c3dcba11c8ba7190
sefeoluwa <sefeoluwa01@gmail.com>
2023-04-13 11:33:02 +0100

    dom elements selected in js file

---
+++ script.js
@@ -0,0 +1,7 @@
+const bookDisplay = document.querySelector('.book-display');
+const btn = documenent.querySelector('add-book');
+
+//add event listener to load a prompt when the button is clicked
+btn.addEventListener('click', function(){
+    
+})
