8d1ec08921f17053153f82eca5365b1041e3f97c
sefeoluwa <sefeoluwa01@gmail.com>
2023-04-16 10:20:54 +0100

    button bug fix :bug:

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const pageNo = document.getElementById('pages');
 const readQ = document.getElementById('read');
 const submitBtn = document.getElementsByClassName('submit-btn');
 const displayContainer = document.getElementById('display');
-
 //declare book object
 class Book {
   constructor(
@@ -22,10 +21,8 @@ class Book {
     this.isRead = isRead;
   }
 }
-
 //set initial display of the form to none
 form.style.display = 'none';
-
 //function to show form and log library with the add book button
 function addBook() {
   addBtn.addEventListener('click', () => {
@@ -33,12 +30,9 @@ function addBook() {
   });
     
 }
-
 addBook();
-
 //create array to store book objects in global scope
 let myLibrary = [];
-
 //use form to add a new book with a function in global scope
 function logBook() {
   event.preventDefault();
@@ -52,7 +46,6 @@ function logBook() {
   displayLibrary();
   form.style.display = 'none';
 }
-
 // function to display the library
 function displayLibrary() {
   displayContainer.innerHTML = '';
@@ -61,7 +54,6 @@ function displayLibrary() {
     //create new elements and settheir inner html to book object values
     const library = document.createElement('div');
     library.classList.add('library');
-
     const titleElement = document.createElement('h2');
     titleElement.textContent = book.title;
     titleElement.classList.add('title-element');
@@ -76,18 +68,39 @@ function displayLibrary() {
     
     const readElement = document.createElement('button');
     readElement.textContent = `Status: ${book.isRead}`;
+    if (readElement.textContent === 'Status: Read'){
+        readElement.classList.add('read-element');
+    } else {
+        readElement.classList.add('unread-element');
+    }
 
-    readElement.addEventListener('click', toggleClass)
-    readElement.addEventListener('touchstart', toggleClass);
-    
+    readElement.addEventListener('click', function(event) {
+        const element = event.target;
+        element.classList.toggle('read-element');
+        element.classList.toggle('unread-element');
+        if (event.target.textContent === 'Status: Read') {
+          event.target.textContent = 'Status: Unread';
+        } else {
+          event.target.textContent = 'Status: Read';
+        }
+      });
+
+     
+
     //remove a book from the library
     const removeBook = document.createElement('button')
     removeBook.classList.add('remove-book');
     removeBook.textContent = "Remove";
+    removeBook.addEventListener('click', () => {
+        const bookElement = event.target.parentNode; // get the parent element of the clicked 'remove' button
+        const bookIndex = Array.from(bookElement.parentNode.children).indexOf(bookElement); // get the index of the book element within its parent
+        myLibrary.splice(bookIndex, 1); // remove the book object from the array
+        bookElement.remove(); // remove the book element from the DOM
+    } )
     removeBook.addEventListener('click', remove)
     removeBook.addEventListener('touchstart', remove);
 
-    
+
 
     //append the new elements to the library
     library.appendChild(titleElement);
@@ -98,23 +111,24 @@ function displayLibrary() {
     displayContainer.appendChild(library);
   });
 }
-
 form.addEventListener('submit', logBook);
 
-//function to toggle classlist
 function toggleClass(event) {
-    const element = event.target;
-    element.classList.toggle('read-element');
-    if (event.target.textContent === 'Status: Read') {
-        element.classList.toggle('unread-element');
-        event.target.textContent = 'Status: Unread';
-      } else {
-        element.classList.toggle('read-element');
-        event.target.textContent = 'Status: Read';
-      }
+    const element = event.currentTarget;
+    if (element.textContent === 'Status: Read') {
+      element.classList.remove('unread-element');
+      element.classList.add('read-element');
+      element.textContent = 'Status: Unread';
+    } else {
+      element.classList.remove('read-element');
+      element.classList.add('unread-element');
+      element.textContent = 'Status: Read';
+    }
   }
   
-  //fucntion to remeove a book
+
+
+  //fucntion to remove a book
 function remove() {
     const bookElement = event.target.parentNode; // get the parent element of the clicked 'remove' button
         const bookIndex = Array.from(bookElement.parentNode.children).indexOf(bookElement); // get the index of the book element within its parent
